1da7e7c7dd1720fab3b44faebb384b8ed751b11f
Daham <Daham@daham-pc>
2015-03-06 06:18:10 -0800

    Did some deletings of comments on UDPClient.java

--- MusicSharingNode/src/com/musicsharing/utilsImp/UDPClient.java
+++ MusicSharingNode/src/com/musicsharing/utilsImp/UDPClient.java
@@ -12,50 +12,6 @@ import com.musicsharing.utils.SocketClient;
 public class UDPClient implements SocketClient {
 	private final static int PACKETSIZE = 100;
 
-//	public static void main(String args[]) {
-//		// Check the arguments
-//		if (args.length != 2) {
-//			System.out.println("usage: java DatagramClient host port");
-//			return;
-//		}
-//
-//		DatagramSocket socket = null;
-//
-//		try {
-//			// Convert the arguments first, to ensure that they are valid
-//			InetAddress host = InetAddress.getByName(args[0]);
-//			int port = Integer.parseInt(args[1]);
-//
-//			// Construct the socket
-//			socket = new DatagramSocket();
-//
-//			// Construct the datagram packet
-//			byte[] data = "Hello Server".getBytes();
-//			DatagramPacket packet = new DatagramPacket(data, data.length, host,
-//					port);
-//
-//			// Send it
-//			socket.send(packet);
-//
-//			// Set a receive timeout, 2000 milliseconds
-//			socket.setSoTimeout(2000);
-//
-//			// Prepare the packet for receive
-//			packet.setData(new byte[PACKETSIZE]);
-//
-//			// Wait for a response from the server
-//			socket.receive(packet);
-//
-//			// Print the response
-//			System.out.println(new String(packet.getData()));
-//
-//		} catch (Exception e) {
-//			System.out.println(e);
-//		} finally {
-//			if (socket != null)
-//				socket.close();
-//		}
-//	}
 
 	@Override
 	public String callAndGotResponse(String server, int portNumber,
